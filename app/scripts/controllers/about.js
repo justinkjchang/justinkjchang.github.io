d21d15f1e6abaad9d89551ef194e1644840497da
Justin <justin.kj.chang@gmail.com>
2015-11-17 06:27:40 -0500

    Initialize AngularJS scaffolding from yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justinkjchanggithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the justinkjchanggithubioApp
+ */
+angular.module('justinkjchanggithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
