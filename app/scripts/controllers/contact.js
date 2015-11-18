e4aeeb152055543732283bb1baa2b412919d85e5
Justin <justin.kj.chang@gmail.com>
2015-11-17 20:06:53 -0500

    Added LHS youtube-like nav menu

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justinkjchanggithubioApp.controller:AboutCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the justinkjchanggithubioApp
+ */
+angular.module('justinkjchanggithubioApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
